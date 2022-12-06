10fb8b2089531f2b5570b19754c01718c375ee10
Shahed1998 <shahedchowdhury120@gmail.com>
2022-12-06 15:32:34 +0600

    ...

---
+++ server/DAL/Interfaces/IRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepo<TClass, TDataType>
+    {
+        TDataType Add(TClass @obj);
+
+        List<TClass> Get();
+
+        TClass Get(int id);
+
+        TDataType Update(TClass @obj);
+
+        TDataType Delete(int id);
+
+
+    }
+}
